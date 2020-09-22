1614e75ee3a1c9af3f86c0121f68119656a47376
baoqianqian <13021861698>
2020-09-22 11:54:05 +0800

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,14 @@
   <div id="app">
     <!-- <h1>自己的项目</h1> -->
     <router-view />
-    <div class="box">hello 狗禹</div>
+    <div class="box">hello {{aaa}}</div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      aaa1: "123",
-      aaa2: "123",
-      aaa3: "123",
+      aaa: "憨憨",
     }
   },
 }
