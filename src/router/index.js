c04b16532d434812031c3d321a9ccf5d22a64dbf
Baoqianqian <2107624791@qq.com>
2020-09-22 23:09:59 +0800

    启动页完成之后，默认显示首页（重定向）

--- src/router/index.js
+++ src/router/index.js
@@ -3,45 +3,44 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 // 路由注册表
-const routes = [
-  {
-    path:'/',
-    name:'start',
-    component:()=> import ('@/views/start/index.vue')
+const routes = [{
+    path: '/',
+    name: 'start',
+    component: () => import('@/views/start/index.vue')
   },
   {
-    path:"/login",
-    name:"login",
-    component:()=> import ('@/views/login/index.vue')
+    path: "/login",
+    name: "login",
+    component: () => import('@/views/login/index.vue')
   },
   {
-    path:"/lay",
-    component:()=> import ('@/views/lay/index.vue'),
-    children:[
-      {
-        path:'/home',
-        name:"home",
-        component:()=> import ('@/views/home/index.vue')
+    path: "/lay",
+    redirect: "home",
+    component: () => import('@/views/lay/index.vue'),
+    children: [{
+        path: '/home',
+        name: "home",
+        component: () => import('@/views/home/index.vue')
       },
       {
-        path:'/qa',
-        name:"qa",
-        component:()=> import ('@/views/qa/index.vue')
+        path: '/qa',
+        name: "qa",
+        component: () => import('@/views/qa/index.vue')
       },
       {
-        path:'/video',
-        name:"video",
-        component:()=> import ('@/views/video/index.vue')
+        path: '/video',
+        name: "video",
+        component: () => import('@/views/video/index.vue')
       },
       {
-        path:'/my',
-        name:"my",
-        component:()=> import ('@/views/my/index.vue')
+        path: '/my',
+        name: "my",
+        component: () => import('@/views/my/index.vue')
       },
     ]
   }
 ];
- 
+
 
 
 const router = new VueRouter({
