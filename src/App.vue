3af15e94c168ccb0b12a43b93afa1f06543fff48
Baoqianqian <2107624791@qq.com>
2020-09-26 21:40:39 +0800

    我的（登录注册）页面

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,25 @@ export default {
 }
 </script>
 <style lang="scss">
-  #app{
-    height: 100%;
-    overflow: hidden;
-  }
+#app {
+  height: 100%;
+  overflow: hidden;
+}
+.fl {
+  float: left;
+}
+.fr {
+  float: right;
+}
+.clearfix::after {
+  content: ""; /*内容为空*/
+  display: block; /*显示为块状元素*/
+  width: 0; /*宽0*/
+  height: 0; /*高0*/
+  visibility: hidden; /*隐藏*/
+  clear: both; /*左右不允许有浮动元素（左右浮动都清除）*/
+}
+.clearfix {
+  zoom: 1;
+}
 </style>
